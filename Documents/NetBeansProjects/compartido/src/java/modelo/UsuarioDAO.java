b9b1156570aca7de849035e9a60c3f92cb39b489
raranda3 <raranda3@misena.edu.co>
2015-01-11 11:50:43 -0500

    proyecto de prueba pra compatir con mac

---
+++ Documents/NetBeansProjects/compartido/src/java/modelo/UsuarioDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author UsuarioDAO
+ * @author Rodrigo Aranda Fernandez
+ */
+public class UsuarioDAO {
+
+}
