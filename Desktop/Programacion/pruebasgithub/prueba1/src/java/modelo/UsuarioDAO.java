decdd84031b1df26fab1e6668ab4d813a7ed1791
raranda3 <raranda3@misena.edu.co>
2015-01-12 00:01:43 -0500

    proyecto de prueba 1 para compartir

---
+++ Desktop/Programacion/pruebasgithub/prueba1/src/java/modelo/UsuarioDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author UsuarioDAO
+ * @author Rodrigo Aranda Fernandez
+ */
+public class UsuarioDAO {
+
+    private String userName ="Rodrigo";
+    private String pass = "abc123";
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the pass
+     */
+    public String getPass() {
+        return pass;
+    }
+
+    /**
+     * @param pass the pass to set
+     */
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+
+}
